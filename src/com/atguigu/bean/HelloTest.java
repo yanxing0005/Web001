9c8f9d0a0a2acf630aa10cbbe812420f045330a4
yanxing <2632281476@qq.com>
2020-02-13 18:59:19 +0800

    Initial commit

---
+++ src/com/atguigu/bean/HelloTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloTest {
+    public static void main(String[] args) {
+        List list=new ArrayList();
+    }
+
+}
